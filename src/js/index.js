dc1f6a7248f2fbd9c2cee6cab1b248910a11e308
Víctor Auñón <hola@victoraunon.com>
2022-02-23 16:35:17 +0100

    Navigation bar

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 import "../css/normalize.css";
+import "../css/styles.scss";
+
+// Display services links in the navigation bar
+const servicesBtn = document.querySelector("#services-button");
+servicesBtn.addEventListener("click", () => {
+  document.querySelector("#services-dropdown").classList.toggle("show");
+});
+
+window.onclick = (event) => {
+  if (event.target.id !== "services-button") {
+    const dropdown = document.querySelector("#services-dropdown");
+    if (dropdown.classList.contains("show")) {
+      dropdown.classList.remove("show");
+    }
+  }
+};
