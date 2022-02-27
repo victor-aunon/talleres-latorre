32fa00ecdfbf9f44433033f8b9814caa965476da
Víctor Auñón <hola@victoraunon.com>
2022-02-27 16:04:04 +0100

    Home page: services, repairing jobs images, map and facebook

--- src/js/index.js
+++ src/js/index.js
@@ -55,4 +55,57 @@ document.addEventListener("DOMContentLoaded", () => {
       coverSubtitle.style.animationName = "element_appear";
     }, 1600);
   }
+
+  // Image comparison
+  const sliders = document.querySelectorAll(".comparison-img-slider");
+  if (sliders.length > 0) {
+    const images = document.querySelectorAll(".comparison-img-overlay");
+    const tooltips = document.querySelectorAll(".comparison-tooltip");
+    const tooltipObservers = [];
+    // Put slider and images at 50%
+    for (let i = 0; i < sliders.length; i++) {
+      images[i].style.clipPath =
+        "polygon(0 0," +
+        sliders[i].value +
+        "% 0," +
+        sliders[i].value +
+        "% 100%, 0 100%)";
+
+      // Set function when moving the slider
+      sliders[i].oninput = () => {
+        images[i].style.clipPath =
+          "polygon(0 0," +
+          sliders[i].value +
+          "% 0," +
+          sliders[i].value +
+          "% 100%, 0 100%)";
+      };
+
+      // Hide the tooltips when they are visible
+      const observer = new window.IntersectionObserver(
+        (entry) => {
+          if (entry[0].isIntersecting) {
+            setTimeout(() => {
+              tooltips[i].style.animation =
+                "element_disappear 1.5s linear forwards";
+            }, 1800);
+          } else {
+            tooltips[i].style.animation = "pulse 2s infinite";
+          }
+        },
+        {
+          threshold: 1,
+          root: null,
+          rootMargin: "0px 0px 50px 0px",
+        }
+      );
+      observer.observe(tooltips[i]);
+      tooltipObservers.push(observer);
+    }
+  }
+
+  // Replace copyright year
+  const copyYear = document.querySelector('#copyright-year');
+  const now = new Date();
+  copyYear.textContent = now.getFullYear();
 });
